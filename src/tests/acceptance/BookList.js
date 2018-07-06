7b8da1c8d6450386fe360bc4cbff74da3992589c
Felipe Borges Ferreira <lypborges@gmail.com>
2018-07-06 10:26:45 -0300

    Remove acceptance test

---
+++ src/tests/acceptance/BookList.js
@@ -0,0 +1,11 @@
+// TODO
+// import visit from "../helpers/visit";
+//
+// describe("When visiting the homepage", function() {
+//   test("it welcomes the user", async function() {
+//     let page = visit("/");
+//
+//     let text = await page.evaluate(() => document.body.textContent).end();
+//     expect(text).toContain("My reads");
+//   });
+// });
